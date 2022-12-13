e29be09e3489a0d23eaca64c1f0d49d55bbf3d10
aldimdiansyah <aldimdiansyah@gmail.com>
2022-12-13 09:58:55 +0700

    membuat game  dengan javascript

---
+++ pertemuan11/script.js
@@ -0,0 +1,75 @@
+let batu = document.getElementById("batu");
+let gunting = document.getElementById("gunting");
+let kertas = document.getElementById("kertas");
+let computerChoice = document.getElementById("computerChoice");
+let options = ["batu", "gunting", "kertas"];
+
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+batu.addEventListener('click', function(){
+    let random = Math.floor(Math.random() * 3)
+    let randomOption = options[random];
+    computerChoice.innerHTML = capitalizeFirstLetter(randomOption);
+
+    setTimeout(() => {
+        switch (randomOption) {
+            case "batu":
+                alert("Seri");
+                break;
+            
+            case "gunting":
+                alert("Kamu Menang!");
+                break;
+        
+            default:
+                alert("Komputer Menang!");
+                break;
+        }
+    }, 300);
+})
+
+gunting.addEventListener('click', function(){
+    let random = Math.floor(Math.random() * 3)
+    let randomOption = options[random];
+    computerChoice.innerHTML = capitalizeFirstLetter(randomOption);
+
+    setTimeout(() => {
+        switch (randomOption) {
+            case "batu":
+                alert("Komputer Menang!");
+                break;
+            
+            case "gunting":
+                alert("Seri");
+                break;
+        
+            default:
+                alert("Kamu Menang!");
+                break;
+        }
+    }, 300);
+})
+
+kertas.addEventListener('click', function(){
+    let random = Math.floor(Math.random() * 3)
+    let randomOption = options[random];
+    computerChoice.innerHTML = capitalizeFirstLetter(randomOption);
+
+    setTimeout(() => {
+        switch (randomOption) {
+            case "batu":
+                alert("Kamu Menang!");
+                break;
+            
+            case "gunting":
+                alert("Komputer Menang!");
+                break;
+        
+            default:
+                alert("Seri");
+                break;
+        }
+    }, 300);
+})
